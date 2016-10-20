96cdc4462602dac0bce97e3d139812ba3d53c368
alekzonder <alekzonder@gmail.com>
2016-10-20 19:54:03 +0700

    deps: update nprof, integrate nprof/express/register

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('maf/vendors/express');
 var logger = require('maf/Service/Logger')('mazaid');
 
-var nprof = require('nprof');
+var nprofExpressRegister = require('nprof/express/register');
 
 process.on('unhandledRejection', (error) => {
     logger.fatal(error);
@@ -30,103 +30,7 @@ require(path.join(__dirname, '/init/config'))()
             res.json(di.config.feConfig);
         });
 
-        app.post('/_profile/cpu', function (req, res) {
-            nprof.cpuProfile(di.config.nprof.snapshotPath, 5000)
-                .then((info) => {
-                    res.json({result: info});
-                })
-                .catch((error) => {
-                    logger.error(error);
-                    res.status(500).json({error: error.message});
-                });
-        });
-
-        app.post('/_profile/cpu/start', function (req, res) {
-
-            logger.info('[POST /_profile/cpu/start] starting cpu profile');
-
-            nprof.startCpuProfile();
-            res.json({result: 'started'});
-        });
-
-        app.post('/_profile/cpu/stop', function (req, res) {
-
-            logger.info('[POST /_profile/cpu/stop] stopping cpu profile');
-
-            var profile = nprof.stopCpuProfile();
-
-            nprof.saveCpuProfile(profile, di.config.nprof.snapshotPath)
-                .then((info) => {
-                    logger.info('[POST /_profile/cpu/stop] profile saved to ' + info.filepath);
-                    res.json({result: info});
-                })
-                .catch((error) => {
-                    logger.error(error);
-                    res.status(500).json({error: error.message});
-                });
-
-        });
-
-        app.post('/_profile/mem/snapshot', function (req, res) {
-
-            logger.info('[POST /_profile/mem] taking memory snapshot');
-
-            nprof.takeMemorySnapshot(di.config.nprof.snapshotPath)
-                .then((info) => {
-                    logger.info('[POST /_profile/mem] memory snapshot saved to ' + info.filepath);
-                    res.json({result: info});
-                })
-                .catch((error) => {
-                    logger.error(error);
-                    res.status(500).type('json').json({error: error.message});
-                });
-        });
-
-        app.get('/_profile/mem/usage', function (req, res) {
-            var raw = process.memoryUsage();
-
-            var mem = {};
-
-            if (req.query.scale) {
-
-                var scale = parseInt(req.query.scale);
-
-                for (var key in raw) {
-                    mem[key] = raw[key] / scale;
-                }
-
-            } else {
-                mem = raw;
-            }
-
-            res.json({
-                result: mem
-            });
-        });
-
-        app.post('/_profile/gc/start', function (req, res) {
-
-            if (!global.gc) {
-                this._logger.warn('no global.gc');
-                return res.status(400).json({error: 'no global.gc'});
-            }
-
-            logger.info('[/_profile/gc/start] starting gc');
-
-            var before = process.memoryUsage();
-
-            global.gc();
-
-            var after = process.memoryUsage();
-
-            res.json({
-                result: {
-                    before: before,
-                    after: after
-                }
-            });
-
-        });
+        nprofExpressRegister(logger, app, di.config.nprof);
 
         // if _debug
         app.use((req, res, next) => {
